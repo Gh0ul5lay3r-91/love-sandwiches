60589229c8516eedb590ce9e37dcf88920e1b2a1
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2021-09-27 22:50:18 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -37,7 +37,8 @@ def get_sales_data():
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into an int, or if there arnt exactly 6 values
+    Raises ValueError if strings cannot be converted into an int, 
+    or if there arnt exactly 6 values
     """
     try: 
         [int(value) for value in values]
@@ -95,6 +96,20 @@ def get_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 def main():
     """
     Run all function programs
@@ -105,7 +120,9 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to love sandwiches data automation")
-#main()
-sales_columns = get_last_5_entries_sales()
+main()
